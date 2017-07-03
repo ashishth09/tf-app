c5e9b4a19feac0dd0f31ae0f2f8465e72dac63e0
Ashish Kumar Thakur <ashishth@in.ibm.com>
2017-07-03 17:48:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ bluemix_api_key = "${var.bluemix_api_key}"
 
 resource "null_resource" "prepare_app_zip" {
   triggers = {
-    app_version = "88"
+    app_version = "100"
     git_repo = "${var.git_repo}"
   }
   provisioner "local-exec" {
