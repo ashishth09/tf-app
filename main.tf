c6c42798cca0e814a912a0cd303ea980057bc75a
Ashish Kumar Thakur <ashishth@in.ibm.com>
2017-08-22 15:15:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,7 @@ provider "ibm" {
 bluemix_api_key = "${var.bluemix_api_key}" 
 }	
 
-module "consul" {
-  source  = "github.com/hashicorp/consul/terraform/aws"
-  servers = 3
-}
-  
-
-
 data "ibm_space" "space" {
   org   = "${var.org}"
   space = "${var.space}"
 }
-
-resource "ibm_service_instance" "service" {
-  name       = "${var.service_instance_name}"
-  space_guid = "${data.ibm_space.space.id}"
-  service    = "${var.service_offering}"
-  plan       = "${var.plan}"
-  tags       = ["my-service"]
-}
-
