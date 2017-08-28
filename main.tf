dd4b535063f598437990911b1b2ec2a2c7ecbce9
Ashish Kumar Thakur <ashishth@in.ibm.com>
2017-08-29 00:28:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,11 +17,10 @@ resource "ibm_app_route" "route" {
   space_guid  = "${data.ibm_space.space.id}"
   host        = "ashish-host"
 }
-/*
+
 //Repeated to create error
 resource "ibm_app_route" "route1" {
   domain_guid = "${data.ibm_app_domain_shared.domain.id}"
   space_guid  = "${data.ibm_space.space.id}"
   host        = "ashish-host"
 }
-*/
